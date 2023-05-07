3b72e148a8b8069bb7660969163da6f552439ed3
Adams Fontil <adamsfuntil@gmail.com>
2023-05-07 12:17:35 -0400

    make the quicklinks component responsive and a slides to platters component only thing that needs responsiveness is header

--- src/Components/PopularPlatters.js
+++ src/Components/PopularPlatters.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Image1 from '../Assets/backendPics/Produce/grapes.jpg'
 import Image2 from '../Assets/backendPics/Produce/bananas.jpg'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 const popularPlattersData = [
   {
@@ -43,28 +46,61 @@ const popularPlattersData = [
 ]
 
 const PopularPlatters = () => {
+  const settings = {
+    dots: true,
+    progressBar: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  }
   return (
-    <div className='m-5 hidden md:block'>
+    <div className='flex-col p-5 gap-5 m-5 hidden md:flex'>
       <div className='flex justify-between'>
-        <h2>Popular Platters</h2>
-        <button className='green-400'>View all</button>
+        <h2 className='text-2xl text-gray-700'>Popular Platters</h2>
+        <button className='bg-green-600 border bg-opacity-1 rounded-md text-green-900 p-2'>View all</button>
       </div>
 
-      <div className='flex mx-8 justify-start ' style={{ height: '' }}>
-        {popularPlattersData.map((platter, index) => (
-          <div className=' flex flex-col justify-evenly flex-1  gap-5' key={index}>
-            <img className='w-32 object-contain' src={platter.imageUrl} alt={platter.name} />
-            <div className=''>
-              <p className='text-small'>{platter.name}</p>
-              {platter.description && <p>{platter.description}</p>}
-              {platter.notice && <p>{platter.notice}</p>}
+      <div className='flex flex-col p-4 '>
+        <Slider { ...settings}>
+          {popularPlattersData.map((platter, index) => (
+            <div className='grid grid-cols-1 grid-rows-4 justify-end items-end rounded-md shadow-md gap-12 p-4 ' key={index}>
+              <div>
+                <img className='object-cover h-52 w-96 rounded-t-md' src={platter.imageUrl} alt={platter.name} />
+              </div>
+
+              <div className='flex  py-4 flex-col'>
+                <p className=''>{platter.name}</p>
+                {platter.description && <p>{platter.description}</p>}
+                {platter.notice && <p>{platter.notice}</p>}
+              </div>
+              <div className='py-4'>
+                <p>{platter.price}</p>
+              </div>
+              <div className=''>
+                <button className='bg-green-600 text-gray-100 py-2 px-4 rounded-md'>{platter.button}</button>
+              </div>
             </div>
-            <div className=''>
-              <p>{platter.price}</p>
-              <button className='bg-green-600 rounded-sm text-white p-1'>{platter.button}</button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </Slider>
       </div>
     </div>
   )
--- src/Components/Quicklinks.js
+++ src/Components/Quicklinks.js
@@ -1,25 +1,32 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 
 function Quicklinks() {
   return (
-    <div className='bg-green-800 flex flex-row justify-end'>
-      <ul className="flex">
-        <li className="mx-2">
-          <a href="#" className="text-white underline">Catering</a>
-        </li>
-        <li className="mx-2">
-          <a href="#" className="text-white underline">Gift Cards</a>
-        </li>
-        <li className="mx-2">
-          <a href="#" className="text-white underline">Order Sushi</a>
-        </li>
-        <li className="mx-2">
-          <a href="#" className="text-white underline">Order Subs</a>
-        </li>
-        <li className="mx-2">
-          <a href="#" className="text-white underline">Weekly Ad</a>
-        </li>
-      </ul>
+    <div className='bg-green-800 flex flex-row md:justify-end'>
+      <div className='flex text-xs text-white gap-1 p-2 md:hidden'>
+        <p>You're shopping at </p>
+        <p className='underline'>Publix at North Miami</p>
+      </div>
+      <div className='hidden md:block'>
+        <ul className="flex">
+          <li className="mx-2">
+            <a href="#" className="text-white underline">Catering</a>
+          </li>
+          <li className="mx-2">
+            <a href="#" className="text-white underline">Gift Cards</a>
+          </li>
+          <li className="mx-2">
+            <a href="#" className="text-white underline">Order Sushi</a>
+          </li>
+          <li className="mx-2">
+            <a href="#" className="text-white underline">Order Subs</a>
+          </li>
+          <li className="mx-2">
+            <a href="#" className="text-white underline">Weekly Ad</a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
